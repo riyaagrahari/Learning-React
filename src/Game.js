ebd40bead34e66d7e2926c99c4aff169e65f03ab
riyaagrahari <riyaagrahari121@gmail.com>
2020-05-22 00:48:53 +0530

    reorganized code structure

--- src/Game.js
+++ src/Game.js
@@ -4,9 +4,12 @@ import { useState } from "react";
 function Button()
 {
     const [counter, setCounter] = useState(1);
+    const handleClick = () => {setCounter(counter*2)}
     return(
-    (<button style ={{width:'100px', height:'100px', position:"absolute", left: '350px', top: '300px' }} onClick = {() => setCounter(counter*2)}>{counter}</button>)
-    )
+    <button style ={{width:'100px', height:'100px', position:"absolute", left: '350px', top: '300px', backgroundColor: '#008CBA' }} onClick = {handleClick}>
+        {counter}
+    </button>
+    );
 }
 
 export default Button
